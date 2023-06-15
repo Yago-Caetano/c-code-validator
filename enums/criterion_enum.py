b31928c39fcc5bbdc3a9037fc42a58b1cfb5a6eb
Yago-Caetano <yago_caetano@outlook.com>
2023-06-15 20:22:04 -0300

    refc - criterionEnum and rule_model

--- enums/criterion_enum.py
+++ enums/criterion_enum.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 class CriterionEnum(Enum):
+    NONE = "none"
     LENGTH_LESS_THAN = "length_less_than"
     LENGTH_BIGGER_THAN = "length_bigger_than"
     PREFIX = "prefix"
--- models/rule_model.py
+++ models/rule_model.py
@@ -1,12 +1,19 @@
 
+from enums.criterion_enum import CriterionEnum
 from enums.target_enum import TargetEnum
 
 
-class rule_model:
+class RuleModel:
 
-    def __init__(self) -> None:
+    def __init__(self,target: TargetEnum, criterion: CriterionEnum) -> None:
         self.__target = TargetEnum.TARGET_NONE
-        
+        self.__criterion = CriterionEnum.NONE
+
+    def get_criterion(self) -> CriterionEnum:
+        return self.__criterion
+    
+    def get_target(self) -> TargetEnum:
+        return self.__target
 
     def check():
         pass
